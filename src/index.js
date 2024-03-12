1491576df97a7f183b6bf8155fc8a5e8a3e829c5
xusp <xspstudyemail@163.com>
2024-03-12 09:51:09 +0800

    feat: use mocha and chai to achieve unit test.

--- src/index.js
+++ src/index.js
@@ -3,3 +3,14 @@ const str = "some value";
 function fn() {
   console.log("some log");
 }
+
+const add = (a, b) => {
+  if (typeof a === "number" && typeof b === "number") {
+    return a + b;
+  }
+  return NaN;
+};
+
+module.exports = {
+  add,
+};
