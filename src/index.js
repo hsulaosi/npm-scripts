869f525310fe50fe46389cd948d0d5b3e6bbeb6b
xusp <xspstudyemail@163.com>
2024-03-19 14:27:29 +0800

    test: husky pre-commit hook.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-const str = 'some value';
+const str = 'some value'
 
 function fn() {
-  console.log('some log');
+  console.log('some log')
 }
 
 const add = (a, b) => {
   if (typeof a === 'number' && typeof b === 'number') {
-    return a + b;
+    return a + b
   }
-  return NaN;
-};
+  return NaN
+}
 
 module.exports = {
   add,
-};
+}
